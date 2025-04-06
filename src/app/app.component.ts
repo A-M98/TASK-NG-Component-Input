12b62902e20222b5957edb24f85a21e6b99b4281
A-M98 <aalmohammad@pifss.gov.kw>
2025-04-06 12:08:22 +0300

    Ahmed Mohammed & Abdullah Al-Kharji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { pets } from '../data/pets';
+import { CatComponent } from './cat/cat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CatComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'TASK-NG-Passing-Data';
--- src/data/pets.ts
+++ src/data/pets.ts
@@ -9,25 +9,28 @@ export const pets: Pet[] = [
   {
     id: 1,
     name: 'Cat',
-    description: 'A curious and independent feline, known for its agility and affection.',
-    image: 'assets/images/cat.jpg'
+    description:
+      'A curious and independent feline, known for its agility and affection.',
+    image: 'assets/images/cat.jpg',
   },
   {
     id: 2,
     name: 'Dog',
-    description: 'A loyal and friendly companion, great for families and active lifestyles.',
-    image: 'assets/images/dog.jpg'
+    description:
+      'A loyal and friendly companion, great for families and active lifestyles.',
+    image: 'assets/images/dog.jpg',
   },
   {
     id: 3,
     name: 'Rabbit',
     description: 'A gentle and quiet pet that loves to nibble and hop around.',
-    image: 'assets/images/rabbit.jpg'
+    image: 'assets/images/rabbit.jpg',
   },
   {
     id: 4,
     name: 'Otter',
-    description: 'An energetic and playful animal, often seen swimming and sliding.',
-    image: 'assets/images/otter.jpg'
-  }
+    description:
+      'An energetic and playful animal, often seen swimming and sliding.',
+    image: 'assets/images/otter.jpg',
+  },
 ];
